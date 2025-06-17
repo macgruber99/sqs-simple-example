db6464b7dea2a48de3f9bcca4abc2e5c7a0b7180
Erik Green <erikgreen@gmail.com>
2025-06-17 16:56:26 -0700

    feat: use Terraform S3 backend

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,10 +1,18 @@
 terraform {
-  required_version = "~> 1.0"
+  required_version = ">= 1.9.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
+
+  backend "s3" {  
+    bucket       = "eriks-terraform-state"  
+    key          = "sqs-simple-example/terraform.tfstate"  
+    region       = "us-west-2"  
+    encrypt      = true  
+    use_lockfile = true  # S3 native locking
   }
 }
