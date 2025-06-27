3f3d47c6a8aea601f07a0ee340452e99856ca010
Erik Green <erikgreen@gmail.com>
2025-06-27 09:32:56 -0700

    feat: configure S3 trigger for producer lambda

--- lambdas/producer/src/producer/lambda_function.py
+++ lambdas/producer/src/producer/lambda_function.py
@@ -2,22 +2,13 @@
 import json
 import os
 
-from datetime import datetime
-from datetime import timezone
+from urllib.parse import unquote_plus
 
 # Third-party library imports
 import boto3
-import lorem
 
+from botocore.exceptions import ClientError
 from aws_lambda_powertools import Logger
-
-
-def get_datetime():
-    """
-    Returns the current date and time as a string in ISO 8601 format.
-    """
-
-    return datetime.now(timezone.utc).isoformat()
 
 
 def get_ssm_parameter(param_name):
@@ -34,6 +25,70 @@ def get_ssm_parameter(param_name):
     )
 
     return response["Parameter"]["Value"]
+
+
+def is_valid_event_source(event, bucket_name):
+    """
+    Checks if the event source is valid for the given S3 bucket.
+
+    :param event: The event data to check.
+    :param bucket_name: The name of the S3 bucket.
+    :return: True if the event source is valid, False otherwise.
+    """
+
+    return (
+        "Records" in event
+        and len(event["Records"]) > 0  # noqa: W503
+        and "s3" in event["Records"][0]  # noqa: W503
+        and event["Records"][0]["s3"]["bucket"]["name"] == bucket_name  # noqa: W503
+    )
+
+
+def is_valid_obj_size(event, max_size):
+    """
+    Checks if the size of the S3 object in the event is within the specified limit.
+
+    :param event: The event data containing the S3 object.
+    :param max_size: The maximum allowed size of the S3 object in bytes.
+    :return: True if the object size is valid, False otherwise.
+    """
+
+    return (
+        "Records" in event
+        and len(event["Records"]) > 0  # noqa: W503
+        and "s3" in event["Records"][0]  # noqa: W503
+        and "object" in event["Records"][0]["s3"]  # noqa: W503
+        and event["Records"][0]["s3"]["object"]["size"] <= max_size  # noqa: W503
+    )
+
+
+def is_valid_json(json_string):
+    """
+    Checks if the provided string is a valid JSON.
+
+    :param json_string: The string to check.
+    :return: True if the string is valid JSON, False otherwise.
+    """
+
+    try:
+        json.loads(json_string)
+        return True
+    except ValueError:
+        return False
+
+
+def read_from_s3(bucket_name, file_name):
+    """
+    Reads the content of a file from an S3 bucket.
+
+    :param bucket_name: The name of the S3 bucket.
+    :param file_name: The name of the file to read.
+    :return: The content of the file as a string.
+    """
+
+    s3 = boto3.client("s3")
+    response = s3.get_object(Bucket=bucket_name, Key=file_name)
+    return response["Body"].read().decode("utf-8")
 
 
 def send_message_to_sqs(message_body, queue_url, message_attributes=None):
@@ -63,10 +118,7 @@ def lambda_handler(event, context):
 
     logger = Logger()
 
-    message = {
-        "text": lorem.sentence(),  # generate random text that looks like Latin
-        "timestamp": get_datetime(),
-    }
+    max_obj_size = 262144  # 256 KB, max size for SQS message
 
     # get the S3 bucket name from SSM Parameter Store
     try:
@@ -80,15 +132,46 @@ def lambda_handler(event, context):
         logger.exception(f"Environment variable SSM_PARAM_BUCKET not set: {e}")
         raise
     except Exception as e:
-        logger.exception(f"Error fetching parameter {ssm_param_path_bucket}: {e}")
+        logger.exception(f"Could not fetch parameter {ssm_param_path_bucket}: {e}")
         raise
 
-    # read from S3 bucket
+    # validate that the event source bucket matches the expected bucket
+    if not is_valid_event_source(event, bucket_name):
+        logger.error(
+            f"Expected event source to contain S3 bucket '{bucket_name}', but got event: {event}"
+        )
+        raise ValueError("Invalid event source.")
+
+    if not is_valid_obj_size(event, max_obj_size):
+        logger.error(
+            f"Object size exceeds maximum allowed size of {max_obj_size} bytes."
+        )
+        raise ValueError("Object size exceeds maximum allowed size.")
+
+    # read object from S3 bucket
     try:
-        print(f"bucket name: {bucket_name}")
-    except Exception as e:
-        logger.exception(f"Error reading from S3 bucket {bucket_name}: {e}")
+        obj_key = event["Records"][0]["s3"]["object"]["key"]
+        obj_key = unquote_plus(obj_key)  # decode URL-encoded key
+        logger.info(f"Reading object '{obj_key}' from S3 bucket '{bucket_name}'.")
+        obj_value = read_from_s3(bucket_name, obj_key)
+    except KeyError as e:
+        logger.exception(f"No S3 object key found in event: {e}")
         raise
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "NoSuchKey":
+            logger.exception(f"S3 object key '{obj_key}' not found: {e}")
+            raise
+        else:
+            # Handle other potential ClientErrors
+            logger.exception(f"Could not read object from bucket: {e}")
+            raise
+    except Exception as e:
+        logger.exception(f"Could not read object from S3 bucket:\n{e}")
+        raise
+
+    if not is_valid_json(obj_value):
+        logger.error(f"Content of object '{obj_value}' is not valid JSON.")
+        raise ValueError("Object content is not valid JSON.")
 
     # get the SQS queue URL from SSM Parameter Store
     try:
@@ -102,15 +185,15 @@ def lambda_handler(event, context):
         logger.exception(f"Environment variable SSM_PARAM_QUEUE not set: {e}")
         raise
     except Exception as e:
-        logger.exception(f"Error fetching parameter {ssm_param_path_queue}: {e}")
+        logger.exception(f"Could not fetch parameter {ssm_param_path_queue}: {e}")
         raise
 
-    # send the message to the SQS queue
+    # send a message to the SQS queue
     try:
         logger.info(f"Sending message to SQS queue '{queue_url}'.")
-        send_message_to_sqs(message, queue_url)
+        send_message_to_sqs(obj_value, queue_url)
     except Exception as e:
-        logger.exception(f"Error sending message to SQS queue '{queue_url}': {e}")
+        logger.exception(f"Could not send message to SQS queue '{queue_url}': {e}")
         raise
 
     logger.info("Done.")
---
+++ scripts/write-to-s3.py
@@ -0,0 +1,53 @@
+# Python Standard Library imports
+import argparse
+import json
+
+from datetime import datetime
+from datetime import timezone
+
+# Third-party library imports
+import boto3
+import lorem
+
+
+def write_obj_to_s3(bucket_name, file_name, message):
+    """
+    Writes content to a file in an S3 bucket.
+
+    :param bucket_name: The name of the S3 bucket.
+    :param file_name: The name of the file to create in the bucket.
+    :param content: The content to write to the file.
+    :return: A dictionary with response data.
+    """
+
+    client = boto3.client("s3")
+    client.put_object(Bucket=bucket_name, Key=file_name, Body=json.dumps(message))
+
+
+def main():
+    """
+    Main function to write a message to an S3 bucket.
+    """
+
+    # define some variables
+    sentence = lorem.sentence()  # generate random text that looks like Latin
+    timestamp = datetime.now(timezone.utc).isoformat()
+    first_word = sentence.split()[0]  # get the first word of the sentence
+    s3_obj_key = f"{first_word}-{timestamp}.json"  # create a unique file name based on the timestamp
+
+    message = {
+        "text": sentence,
+        "timestamp": timestamp,
+    }
+
+    # parse the command line arguments
+    parser = argparse.ArgumentParser(description="Write a message to an S3 bucket.")
+    parser.add_argument("bucket_name", type=str, help="The name of the S3 bucket")
+    args = parser.parse_args()
+
+    # write the message to S3
+    write_obj_to_s3(args.bucket_name, s3_obj_key, json.dumps(message))
+
+
+if __name__ == "__main__":
+    main()
