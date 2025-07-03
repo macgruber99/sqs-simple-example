9349d8599845062b58bf41f7037fa94067394f39
Erik Green <erikgreen@gmail.com>
2025-07-03 16:44:12 -0700

    feat: move CodeBuild Terraform to cicd dir

---
+++ cicd/locals.tf
@@ -0,0 +1,11 @@
+locals {
+  # With merge(), if the same key is defined in both dictionaries, then the one
+  # that is later in the argument sequence takes precedence.  Since the Project
+  # tag should always be present, it is placed last in the merge() call.
+  tags = merge(
+    var.tags,
+    {
+      "Project" = var.project_name,
+    }
+  )
+}
