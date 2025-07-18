2cf972350f0d576e254cf7b70f502faccf426d22
Erik Green <erikgreen@gmail.com>
2025-07-18 13:05:29 -0700

    feat: update script that writes to S3

--- lambdas/consumer/src/consumer/config.py
+++ lambdas/consumer/src/consumer/config.py
@@ -1,6 +1,6 @@
 # the special string that generates an exception
 config = {
-    "special_error_string": "sed error inciderunt.",
+    "special_error_string": "Sed error inciderunt.",
     "ssm_param_path": "/sqs-simple-example",
     "required_ssm_params": ["output-bucket-name", "queue-arn"],
 }
